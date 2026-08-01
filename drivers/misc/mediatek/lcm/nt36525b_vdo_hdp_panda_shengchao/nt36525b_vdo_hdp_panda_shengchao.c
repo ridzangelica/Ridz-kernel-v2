2a22bc2d49a4f54cfa1e70f71ad0bda7d3d8c053
ridz_angelica <ridwantyo9@gmail.com>
2026-08-01 14:21:09 +0700

    Add files via upload
    Add hbm

--- drivers/misc/mediatek/lcm/nt36525b_vdo_hdp_panda_shengchao/nt36525b_vdo_hdp_panda_shengchao.c
+++ drivers/misc/mediatek/lcm/nt36525b_vdo_hdp_panda_shengchao/nt36525b_vdo_hdp_panda_shengchao.c
@@ -20,6 +20,7 @@
 #endif
 
 #include "lcm_drv.h"
+#include "lcm_hbm.h"
 #include "lcm_define.h"
 #include "disp_dts_gpio.h"
 #ifdef BUILD_LK
@@ -538,7 +539,8 @@ static void lcm_setbacklight_cmdq(void *handle, unsigned int level)
 
 	if((0 != level) && (level <= 14))
 		level = 14;
-	level = level*72/100;
+	if (!lcm_hbm_enable)
+		level = level*72/100;
 #ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
 	bl_level[0].para_list[0] = level >> 8;
 	bl_level[0].para_list[1] = level & 0xFF;
