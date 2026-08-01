f3495640987d506dd4d7ae3a0d769e2518a1f266
ridz_angelica <ridwantyo9@gmail.com>
2026-08-01 14:07:07 +0700

    Add files via upload

--- drivers/misc/mediatek/lcm/hx83102d_vdo_hdp_boe_xinli/hx83102d_vdo_hdp_boe_xinli.c
+++ drivers/misc/mediatek/lcm/hx83102d_vdo_hdp_boe_xinli/hx83102d_vdo_hdp_boe_xinli.c
@@ -20,6 +20,7 @@
 #endif
 
 #include "lcm_drv.h"
+#include "lcm_hbm.h"
 #include "lcm_define.h"
 #include "disp_dts_gpio.h"
 #ifdef BUILD_LK
@@ -661,7 +662,8 @@ static void lcm_setbacklight_cmdq(void *handle, unsigned int level)
 {
 
 	pr_debug("[LCM]%s,hx83102d backlight: level = %d\n", __func__, level);
-	level = level*72/100;
+	if (!lcm_hbm_enable)
+		level = level*72/100;
 #ifdef CONFIG_BACKLIGHT_SUPPORT_2047_FEATURE
 	level = level << 1;
 	bl_level[0].para_list[0] = level >> 8;
